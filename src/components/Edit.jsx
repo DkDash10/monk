04a63c2b601af0233a8a7d5abfeed54aa718d610
DkDash10 <dhaneshdash11@gmail.com>
2022-05-27 22:43:49 +0530

    init

---
+++ src/components/Edit.jsx
@@ -0,0 +1,206 @@
+
+import React, { useState, useEffect } from "react";
+import { Checkbox, Modal, styled } from "@mui/material";
+import { Box } from "@mui/system";
+import "./edit.css";
+import { MdEdit } from "react-icons/md";
+import { GrClose, GrSearch } from "react-icons/gr";
+import ReactPaginate from "react-paginate";
+
+const StyledModal = styled(Modal)({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+});
+
+// export const CHECKED = 1;
+// export const UNCHECKED = 2;
+// export const INDETERMINATE = -1;
+
+const Edit = ({childToParent}) => {
+  const [open, setOpen] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [query, setQuery] = useState("");
+
+  const [checkBox, setCheckBox] = useState([])
+
+  // const data= "HElo"
+  const getUsers = async () => {
+    const response = await fetch(
+      "https://stageapibc.monkcommerce.app/admin/shop/product?search=F&page=1"
+    );
+    const data = await response.json();
+    setProducts(data);
+    // console.log(data);
+  };
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  // const handleChange = (e) => {
+  //   const { name, checked } = e.target;
+
+  //   if (name === "allSelect") {
+  //     let tempUser = products.map((data) => {
+  //       return { ...data, isChecked: checked };
+  //     });
+  //     setProducts(tempUser);
+  //   } else {
+  //     // let tempUser = products?.variants.map(products=>products.title === title ? {...products, isChecked: checked}: products
+  //     let tempUser = products?.variants.map((d) =>
+  //       d.name === name ? { ...d, isChecked: checked } : d
+  //     );
+  //     console.log(products.variants.title);
+
+  //     setProducts(tempUser);
+  //   }
+  // };
+
+// const handleChek=(data)=>{
+//   if(checkBox.length === 0){
+//     setCheckBox([{title:data.title, checked:true}])
+//   }else{
+//     checkBox.map(item => item === data ? ( 
+//       setCheckBox(prev=> prev.map(each => each.title === data.title ? {...each, checked:false} : {...each})) 
+//     ) : (
+//       setCheckBox(prev=> prev.map(each => each === data ? {...each} :  {title:data.title, checked:true} ))
+//     ))
+//   }
+// }
+
+const handleChek =(data)=>{
+  const exist = checkBox.find((x) => x.title === data.title);
+  console.log("data")
+  console.log(exist)
+  if(exist){
+    setCheckBox(
+      checkBox.map((x)=>
+      x.title === data.title ? {...exist, checked:!x.checked} : x
+      )
+    );
+  }else{
+    setCheckBox([...checkBox, {title:data.title, checked:true}]);
+  }
+}
+
+const handleChek2 =(d)=>{
+  const exist = checkBox.find((x) => x.title === d.title);
+  console.log("data")
+  console.log(exist)
+  if(exist){
+    setCheckBox(
+      checkBox.map((x)=>
+      x.title === d.title ? {...exist, checked:!x.checked} : x
+      )
+    );
+  }else{
+    setCheckBox([...checkBox, {title:d.title, checked:true}]);
+  }
+}
+
+// const handleChek2=(d)=>{
+//   if (checkBox.includes(d.title) === false){
+//     setCheckBox(prev=>[...prev, d.title])
+//   } 
+//   console.log(d)
+
+// }
+  return (
+    <>
+      <div onClick={(e) => setOpen(true)} className="home__model">
+        <MdEdit className="home__edit-add" />
+      </div>
+      <StyledModal
+        open={open}
+        onClose={(e) => setOpen(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box
+          sx={{ overflowY: "scroll" }}
+          width={600}
+          height={600}
+          bgcolor="white"
+        >
+          <div className="div">
+            <div className="edit__header" varient="h6" fontWeight={700}>
+              Search Products <GrClose onClick={() => setOpen(false)} />
+            </div>
+            <div className="edit__search">
+              <GrSearch className="edit__search-icon" />
+              <input
+                className="edit__input"
+                type="text"
+                placeholder="Search product"
+                onChange={(e) => setQuery(e.target.value)}
+              />
+            </div>
+          </div>
+          <div className="opacity">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque
+            laborum esse accusamus velit sit, voluptates doloremque consequuntur
+            obcaecati rem? Quaerat placeat quod commodi delectus ipsum! Nulla
+            error facilis laudantium amet magnam odio esse, fugiatas Lorem ipsum
+            dolor sit amet consectetur adipisicing elit. Magnam illo, nesciunt
+            earum eveniet inventore adipisci
+          </div>
+
+          {products
+            .filter((data) => data.title.toLowerCase().includes(query))
+            .map((data) => {
+              return (
+                <div key={data.id} className="edit__main-products">
+                  <div className="edit__main-top">
+                    <Checkbox
+                      color="default"
+                      name="allSelect"
+                      onClick={()=>handleChek(data)}
+                      // checked={
+                      //   products.filter((data) => data?.isChecked !== true)
+                      //     .length < 1
+                      // }
+                      // onChange={handleChange}
+                    />
+                    <img
+                      className="edit__main-img"
+                      src={data?.image?.src}
+                      alt=""
+                    />
+                    <p className="edit__main-top-title">{data?.title}</p>
+                  </div>
+                  {data?.variants.map((d) => {
+                    
+                    return (
+                      <div key={d?.id} className="edit__sub-products">
+                        <Checkbox
+                          color="default"
+                          name={d?.title}
+                      onClick={()=>handleChek2(d)}
+
+                          // checked={d?.isChecked || false}
+                          // onChange={handleChange}
+                        />
+                        <div className="edit__sub-info">
+                          <p>{d?.title}</p>
+                          <p>{"$" + d?.price}</p>
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
+              );
+            })}
+          <div className="edit__footer">
+            <p>1 product selected</p>
+            <div>
+            <button className="edit__btn" onClick={() => setOpen(false)}>Cancel</button>
+            <button className="edit__btn-2" onClick={(e) => childToParent(e, checkBox)}>Add</button>
+            </div>
+          </div>
+        </Box>
+      </StyledModal>
+    </>
+  );
+};
+
+export default Edit;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,20 +2,29 @@ import { useState } from "react";
 import { GrClose } from "react-icons/gr";
 import { BiDotsVerticalRounded } from "react-icons/bi";
 import { RiArrowDropDownLine, RiArrowDropUpLine } from "react-icons/ri";
-import { MdEdit } from "react-icons/md";
 // import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
-import Modal from "./Modal";
 import "./home.css";
+import Edit from "./Edit";
 
 function App() {
-  const [openModal, setOpenModal] = useState(false);
-  const [toggle, setToggle] = useState(true);
+  // const [toggle, setToggle] = useState(true);
+  const [data, setData] = useState("");
   const [inputList, setInputList] = useState([
-    { product: "", offer: "", discount: "", id: 1 },
+    {
+      product: "",
+      offer: "",
+      discount: "",
+      addDiscount: true,
+      showVarient: true,
+    },
   ]);
 
-  const handleClick = () => {
-    setToggle(!toggle);
+  const handleClick = (index) => {
+    setInputList((prev) =>
+      prev.map((item, i) =>
+        i === index ? { ...item, showVarient: !item.showVarient } : { ...item }
+      )
+    );
   };
 
   const handleInput = (e, index) => {
@@ -31,154 +40,216 @@ function App() {
     setInputList(list);
   };
 
+  const childToParent = (e, childData) => {
+    e.preventDefault()
+    console.log(e);
+    console.log(childData)
+    childData.map((item,i)=> (i.length=== 0 && inputList.length === 0) ? (
+      setInputList([
+        {
+          product: item.title,
+          offer: "",
+          discount: "",
+          addDiscount: true,
+          showVarient: true,
+        },
+      ])
+    ): (setInputList([
+      ...inputList,
+      {
+        product: item.title,
+        offer: "",
+        discount: "",
+        addDiscount: true,
+        showVarient: true,
+      },
+    ]))  
+ )
+      
+      // if(inputList.length > 1){
+      //   setInputList([
+      //     ...inputList
+      //     {
+      //       product: childData.title,
+      //       offer: "",
+      //       discount: "",
+      //       addDiscount: true,
+      //       showVarient: true,
+      //     },
+      //   ])
+      // }else{
+      //   setInputList([
+      //     {
+      //       product: childData[0].title,
+      //       offer: "",
+      //       discount: "",
+      //       addDiscount: true,
+      //       showVarient: true,
+      //     },
+      //   ])
+      // }
+  }
+           
+    
+
   const handleAdd = () => {
-    setInputList([...inputList, { product: "", offer: "", discount: "", id: id+1 }]);
-    console.log("inputList", inputList )
+    setInputList([
+      ...inputList,
+      {
+        product: "",
+        offer: "",
+        discount: "",
+        addDiscount: true,
+        showVarient: true,
+      },
+    ]);
+    console.log("inputList", inputList);
   };
 
   const handleToggle = (index) => {
-    document.getElementById("toggle").style.display = "block";
-    document.getElementById("togglebtn").style.display = "none";
     console.log(index);
+    setInputList((prev) =>
+      prev.map((item, i) =>
+        i === index ? { ...item, addDiscount: false } : { ...item }
+      )
+    );
+    // document.getElementById("toggle").style.display = "block";
+    // document.getElementById("togglebtn").style.display = "none";
+    //console.log(index);
   };
 
   return (
     <>
       {/* <DragDropContext> */}
-        <div className="home">
-          <h4 className="home__title">Add Products</h4>
-          <div className="home__pd">
-            <h6>Products</h6>
-            <h6>Discount</h6>
-          </div>
-          {/* <Droppable droppableId="body"> */}
-            {/* {(provided) => ( */}
-              {/* <div ref={provided.innerRef} {...provided.droppableProps}> */}
-                {inputList.map((singleService, index) => (
-                  // <Draggable key={index} draggableId={index.name} index={index}>
-                  //   {(provided) => (
-                      <div
-                        key={index+1}
-                        // ref={provided.innerRef}
-                        // {...provided.droppableProps}
-                        className="home__main"
-                      >
-                        <div className="home__input">
-                          <BiDotsVerticalRounded
-                            // {...provided.dragHandleProps}
-                            className="home__drag"
-                          />
-                          <BiDotsVerticalRounded
-                            // {...provided.dragHandleProps}
-                            className="home__drag-1"
-                          />
-                          <p className="sr-no">{index + 1}</p>
-                          <input
-                            className="home__input-main"
-                            name="product"
-                            type="text"
-                            id="product"
-                            value={singleService.product}
-                            onChange={(e) => handleInput(e, index)}
-                            placeholder="Select Products"
-                          />
-                          <div
-                            onClick={() => {
-                              setOpenModal(true);
-                            }}
-                            className="home__model"
-                          >
-                            <MdEdit className="home__edit-add" />
-                            {openModal && <Modal closeModal={setOpenModal}/> }
-                          </div>
-                          <div id="toggle" className="home-input__toggle">
-                            <input
-                              className="home__offer-input"
-                              name="offer"
-                              type="number"
-                              placeholder="20"
-                              onChange={(e) => handleInput(e, index)}
-                              value={singleService.offer}
-                            />
-                            <select
-                              className="home__dropdown"
-                              name="discount"
-                              onChange={(e) => handleInput(e, index)}
-                              value={singleService.discount}
-                            >
-                              <option value="% off">% off</option>
-                              <option value="flat off">flat off</option>
-                            </select>
-                          </div>
-                          <button
-                            id="togglebtn"
-                            onClick={handleToggle}
-                            className="home__discount-btn"
-                          >
-                            Add Discount
-                          </button>
-                          {inputList.length !== 1 && (
-                            <GrClose onClick={() => handleRemove(index)} />
-                          )}
-                        </div>
-                        {toggle ? (
-                          <div className="change">
-                            <button
-                              className="home__varient"
-                              onClick={() => handleClick(index)}
-                            >
-                              Show varient
-                              <RiArrowDropUpLine />
-                            </button>
-                          </div>
-                        ) : (
-                          <div className="change">
-                            <button
-                              className="home__varient"
-                              onClick={handleClick}
-                            >
-                              Hide varient
-                              <RiArrowDropDownLine />
-                            </button>
-                            <div className="home__varient-1">
-                              <input
-                                className="home__varient-input"
-                                type="text"
-                                placeholder="Cotton classic sneakers"
-                              />
-                              <input
-                                className="home__offer-input-2"
-                                name="offer"
-                                type="number"
-                                placeholder="20"
-                              />
-                              <select
-                                className="home__dropdown-2"
-                                name="discount"
-                              >
-                                <option value="% off">% off</option>
-                                <option value="flat off">flat off</option>
-                              </select>
-                              {inputList.length !== 1 && (
-                                <GrClose onClick={() => handleRemove(index)} />
-                              )}
-                            </div>
-                          </div>
-                        )}
-                      </div>
-                    // )}
-                  // </Draggable>
-                ))}
-              {/* </div> */}
-            {/* // )} */}
-          {/* </Droppable> */}
-
-          <div className="home__add-btn">
-            <button type="button" onClick={handleAdd} className="add-btn">
-              Add Products
-            </button>
-          </div>
+      <div className="home">
+        <h4 className="home__title">Add Products</h4>
+        <div className="home__pd">
+          <h6>Products</h6>
+          <h6>Discount</h6>
         </div>
+        {/* <Droppable droppableId="body"> */}
+        {/* {(provided) => ( */}
+        {/* <div ref={provided.innerRef} {...provided.droppableProps}> */}
+        {inputList.map((singleService, index) => (
+          // <Draggable key={index} draggableId={index.name} index={index}>
+          //   {(provided) => (
+
+          <div
+            key={index + 1}
+            // ref={provided.innerRef}
+            // {...provided.droppableProps}
+            className="home__main"
+          >
+            <div className="home__input">
+              <BiDotsVerticalRounded
+                // {...provided.dragHandleProps}
+                className="home__drag"
+              />
+              <BiDotsVerticalRounded
+                // {...provided.dragHandleProps}
+                className="home__drag-1"
+              />
+              <p className="sr-no">{index + 1}</p>
+              <input
+                className="home__input-main"
+                name="product"
+                type="text"
+                id="product"
+                value={singleService.product}
+                onChange={(e) => handleInput(e, index)}
+                placeholder="Select Products"
+              />
+              <Edit childToParent={childToParent} />
+
+              {singleService.addDiscount === true ? (
+                <button
+                  id="togglebtn"
+                  onClick={() => handleToggle(index)}
+                  className="home__discount-btn"
+                >
+                  Add Discount
+                </button>
+              ) : (
+                <div id="toggle" className="home-input__toggle">
+                  <input
+                    className="home__offer-input"
+                    name="offer"
+                    type="number"
+                    placeholder="20"
+                    onChange={(e) => handleInput(e, index)}
+                    value={singleService.offer}
+                  />
+                  <select
+                    className="home__dropdown"
+                    name="discount"
+                    onChange={(e) => handleInput(e, index)}
+                    value={singleService.discount}
+                  >
+                    <option value="% off">% off</option>
+                    <option value="flat off">flat off</option>
+                  </select>
+                </div>
+              )}
+
+              {inputList.length !== 1 && (
+                <GrClose className="home__close" onClick={() => handleRemove(index)} />
+              )}
+            </div>
+            {singleService.showVarient === true ? (
+              <div className="change">
+                <button
+                  className="home__varient"
+                  onClick={() => handleClick(index)}
+                >
+                  Show varient
+                  <RiArrowDropUpLine />
+                </button>
+              </div>
+            ) : (
+              <div className="change">
+                <button
+                  className="home__varient"
+                  onClick={() => handleClick(index)}
+                >
+                  Hide varient
+                  <RiArrowDropDownLine />
+                </button>
+                <div className="home__varient-1">
+                  <input
+                  value={data}
+                    className="home__varient-input"
+                    type="text"
+                  />
+                  <input
+                    className="home__offer-input-2"
+                    name="offer"
+                    type="number"
+                    placeholder="0"
+                  />
+                  <select className="home__dropdown-2" name="discount">
+                    <option value="% off">% off</option>
+                    <option value="flat off">flat off</option>
+                  </select>
+                  {inputList.length !== 1 && (
+                    <GrClose className="home__close-2" onClick={() => handleRemove(index)} />
+                  )}
+                </div>
+              </div>
+            )}
+          </div>
+          // )}
+          // </Draggable>
+        ))}
+        {/* </div> */}
+        {/* // )} */}
+        {/* </Droppable> */}
+
+        <div className="home__add-btn">
+          <button type="button" onClick={handleAdd}  className="add-btn">
+            Add Products
+          </button>
+        </div>
+      </div>
       {/* // </DragDropContext> */}
     </>
   );
